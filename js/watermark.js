703e8b3a908eb531ad7ba2f4282d44f837a4ec25
Corbin Davenport <corbin@dell-xps.hsd1.ga.comcast.net>
2019-04-07 23:20:36 -0400

    Add image import for watermarks

--- js/watermark.js
+++ js/watermark.js
@@ -5,7 +5,7 @@ var globalWatermark = {
     opacity: parseInt(document.getElementById('photostack-watermark-opacity').value),
     horizontalInset: parseInt(document.getElementById('photostack-watermark-horizontal-inset').value),
     veritcalInset: parseInt(document.getElementById('photostack-watermark-vertical-inset').value),
-    anchorPosition: parseInt(document.querySelector('.photostack-anchor-btn.btn-primary').id.replace('photostack-watermark-pos-',''))
+    anchorPosition: parseInt(document.querySelector('.photostack-anchor-btn.btn-primary').id.replace('photostack-watermark-pos-', ''))
 }
 console.log('Default watermark:', globalWatermark)
 
@@ -20,7 +20,7 @@ function updateGlobalWatermark() {
     // Vertical inset
     globalWatermark.veritcalInset = document.getElementById('photostack-watermark-vertical-inset').value
     // Anchor position
-    globalWatermark.anchorPosition = parseInt(document.querySelector('.photostack-anchor-btn.btn-primary').id.replace('photostack-watermark-pos-',''))
+    globalWatermark.anchorPosition = parseInt(document.querySelector('.photostack-anchor-btn.btn-primary').id.replace('photostack-watermark-pos-', ''))
     // Display new settings in console
     console.log('Current watermark:', globalWatermark)
 }
@@ -61,6 +61,75 @@ function loadWatermark() {
     newAnchor.classList.add('btn-primary')
 }
 
+// Add image from local file
+function importLocalImage(file) {
+    // Make sure image file is less than 1MB
+    if (file.size > 1048576) {
+        alert('Watermark must be under 1MB!')
+        return
+    }
+    // Disable file picker while import is in progress
+    document.getElementById('photostack-import-file').disabled = true
+    document.querySelector('label[for="photostack-import-file"]').textContent = 'Importing images...'
+    var image = document.createElement('img')
+    var reader = new FileReader()
+    // Set the image source to the reader result, once the reader is done
+    reader.onload = function () {
+        image.src = reader.result
+    }
+    reader.onerror = function () {
+        alert('Could not import this image: ' + file.name)
+    }
+    // Once both the reader and image is done, we can safely add it to the originals container and clean up
+    image.onload = function () {
+        // Save image to globalWatermark
+        globalWatermark.image = image.src
+    }
+    reader.readAsDataURL(file)
+    // Clear file select
+    document.getElementById('photostack-import-file').value = ''
+    // Re-enable file picker
+    document.getElementById('photostack-import-file').disabled = false
+    document.querySelector('label[for="photostack-import-file"]').textContent = 'Choose image file'
+}
+
+// Add image from URL
+function importExternalImage(url) {
+    // Get image
+    function downloadExternalImage(url) {
+        var image = document.createElement('img')
+        image.crossOrigin = 'anonymous'
+        image.src = url
+        // Create canvas for converting image to Data URL
+        var canvas = document.createElement("canvas")
+        image.onload = function () {
+            console.log('Loaded image URL: ' + url)
+            // Add image to canvas
+            canvas.width = image.naturalWidth
+            canvas.height = image.naturalHeight
+            canvas.getContext('2d').drawImage(image, 0, 0)
+            var data = canvas.toDataURL()
+            // Check file size of image
+            var size = new Blob([data]).size
+            if (size > 1048576) {
+                alert('Watermark must be under 1MB!')
+            } else {
+                // Save image to globalWatermark
+                globalWatermark.image = data
+            }
+        }
+        image.onerror = function () {
+            if (!url.includes('https://cors-anywhere.herokuapp.com/')) {
+                console.log('Error loading image, trying CORS Anywhere...')
+                downloadExternalImage('https://cors-anywhere.herokuapp.com/' + url)
+            } else {
+                alert('Could not import URL.')
+            }
+        }
+    }
+    downloadExternalImage(url)
+}
+
 // Render canvas of preview image
 function renderPreviewCanvas() {
     // Find elements
@@ -97,25 +166,37 @@ for (var i = 0; i < localStorage.length; i++) {
 }
 
 // Load watermark from storage
-document.getElementById('photostack-watermark-select').addEventListener('change', function() {
+document.getElementById('photostack-watermark-select').addEventListener('change', function () {
     loadWatermark()
 })
 
+// Local image picker
+document.getElementById('photostack-import-file').addEventListener('change', function () {
+    importLocalImage(this.files[0])
+})
+
+// URL image picker
+document.getElementById('photostack-import-url-button').addEventListener('click', function () {
+    var url = document.getElementById('photostack-import-url').value
+    importExternalImage(url)
+})
+
+
 // Update globalWatemark when input changes
-document.getElementById('photostack-watermark-size').addEventListener('change', function() {
+document.getElementById('photostack-watermark-size').addEventListener('change', function () {
     updateGlobalWatermark()
 })
-document.getElementById('photostack-watermark-opacity').addEventListener('change', function() {
+document.getElementById('photostack-watermark-opacity').addEventListener('change', function () {
     updateGlobalWatermark()
 })
-document.getElementById('photostack-watermark-horizontal-inset').addEventListener('change', function() {
+document.getElementById('photostack-watermark-horizontal-inset').addEventListener('change', function () {
     updateGlobalWatermark()
 })
-document.getElementById('photostack-watermark-vertical-inset').addEventListener('change', function() {
+document.getElementById('photostack-watermark-vertical-inset').addEventListener('change', function () {
     updateGlobalWatermark()
 })
-document.querySelectorAll('.photostack-anchor-btn').forEach(function(button) {
-    button.addEventListener('click', function() {
+document.querySelectorAll('.photostack-anchor-btn').forEach(function (button) {
+    button.addEventListener('click', function () {
         // Clear .btn-primary style from the currently-active button
         var previousButton = document.querySelector('.photostack-anchor-btn.btn-primary')
         previousButton.classList.remove('btn-primary')
@@ -128,11 +209,12 @@ document.querySelectorAll('.photostack-anchor-btn').forEach(function(button) {
 })
 
 // Watermark selector
-document.getElementById('photostack-save-watermark').addEventListener('click', function() {
+document.getElementById('photostack-save-watermark').addEventListener('click', function () {
     saveWatermark()
 })
-document.getElementById('photostack-delete-watermark').addEventListener('click', function() {
+document.getElementById('photostack-delete-watermark').addEventListener('click', function () {
     alert('Not implemented yet!')
 })
 
+// Render initial preview of canvas
 renderPreviewCanvas()
