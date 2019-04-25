f31632ddeb958763e412e09fd70a57314a6cb753
Corbin Davenport <me@corbin.io>
2019-04-25 17:46:02 -0400

    Add Dropbox export

--- js/export.js
+++ js/export.js
@@ -61,6 +61,49 @@ function createZip() {
             document.getElementById('photostack-export-download-zip-button').addEventListener('click', function() {
                 saveAs(content, 'images.zip')
             })
+            // Save file to Dropbox
+            document.getElementById('photostack-export-download-dropbox-button').addEventListener('click', function() {
+                // Change button text
+                this.disabled = true
+                this.textContent = 'Please wait...'
+                // Generate data URL for Dropbox API
+                zip.generateAsync({ type: 'base64' })
+                    .then(function (content) {
+                        var options = {
+                            // Success is called once all files have been successfully added to the user's
+                            // Dropbox, although they may not have synced to the user's devices yet.
+                            success: function () {
+                                // Indicate to the user that the files have been saved.
+                                alert("Success! Files saved to your Dropbox.");
+                            },
+                        
+                            // Progress is called periodically to update the application on the progress
+                            // of the user's downloads. The value passed to this callback is a float
+                            // between 0 and 1. The progress callback is guaranteed to be called at least
+                            // once with the value 1.
+                            progress: function (progress) {},
+                        
+                            // Cancel is called if the user presses the Cancel button or closes the Saver.
+                            cancel: function () {},
+                        
+                            // Error is called in the event of an unexpected response from the server
+                            // hosting the files, such as not being able to find a file. This callback is
+                            // also called if there is an error on Dropbox or if the user is over quota.
+                            error: function (errorMessage) {}
+                        }
+                        // Create a new button because the popup won't work after the data URL is generated
+                        var newButton = document.createElement('button')
+                        newButton.classList.add('btn', 'btn-block', 'btn-dropbox')
+                        newButton.textContent = 'Confirm save to Dropbox'
+                        newButton.addEventListener('click', function() {
+                            var url = 'data:application/zip;base64,' + encodeURIComponent(content)
+                            Dropbox.save('data:application/zip;base64,' + encodeURIComponent(content), 'images.zip', options)
+                        })
+                        // Replace old button with new button
+                        var oldButton = document.getElementById('photostack-export-download-dropbox-button')
+                        oldButton.parentNode.replaceChild(newButton, oldButton)
+                    })
+            })
         })
 }
 
