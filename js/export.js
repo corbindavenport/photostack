ac534be9f87785f2abba20225f3368e27c0e9200
Corbin Davenport <corbin@pop-os.localdomain>
2019-07-11 01:20:14 -0400

    Start work on async export feature

--- js/export.js
+++ js/export.js
@@ -1,5 +1,5 @@
-// Main export function
-function createZip() {
+// Legacy export function for older browsers
+function legacyExport() {
     // Set variables
     var imgFormat = document.getElementById('photostack-export-format').value
     var imgQuality = parseInt(document.getElementById('photostack-export-quality').value) / 100
@@ -83,6 +83,69 @@ function createZip() {
     })
 }
 
+// Async export with Promises
+function asyncExport() {
+    // Set variables
+    var imgFormat = document.getElementById('photostack-export-format').value
+    var imgQuality = parseInt(document.getElementById('photostack-export-quality').value) / 100
+    var imgNamePattern = document.getElementById('photostack-name-pattern').value
+    if (imgNamePattern === '') {
+        imgNamePattern = 'image'
+    }
+    var imgCount = document.querySelectorAll('#photostack-original-container img').length
+    var progressStep = 100 / imgCount
+    // Switch modal content to progress indicator
+    document.querySelector('.photostack-export-modal-initial').style.display = 'none'
+    // Use jQuery's show() so the progress bar is fully displayed before the image processing begins
+    $('.photostack-export-modal-loading').show('fast', function () {
+        // Start rendering canvases
+        var originals = document.querySelectorAll('#photostack-original-container img')
+        var canvasContainer = document.getElementById('photostack-canvas-container')
+        // Clear current canvas elements
+        canvasContainer.innerHTML = ''
+        // Render canvas for each original image
+        originals.forEach(function (original, i) {
+            // Create canvas element
+            var canvas = document.createElement('canvas')
+            // Add canvas element to canvas container
+            canvasContainer.appendChild(canvas)
+            canvas.width = original.naturalWidth
+            canvas.height = original.naturalHeight
+            canvas.getContext('2d').drawImage(original, 0, 0)
+            // Apply settings
+            applyCanvasSettings(canvas, original)
+        })
+        // Add canvases to ZIP object
+        var zip = new JSZip()
+        var canvases = document.querySelectorAll('#photostack-canvas-container canvas')
+        // Map the array of images to an array of promises
+        var promises = $.map(canvases, function (canvas) {
+            return new Promise(function (resolve) {
+                canvas.toBlob(resolve, imgFormat, imgQuality)
+            })
+        })
+        // When all promises have resolved
+        Promise.all(promises).then(function(blobs) {
+            // Create final array of blobs with file names
+            var exportedFiles = []
+            blobs.forEach(function(blob, i) {
+                if (imgFormat === 'image/jpeg') {
+                    var fileEnding = '.jpg'
+                } else if (imgFormat === 'image/png') {
+                    var fileEnding = '.png'
+                } else if (imgFormat === 'image/webp') {
+                    var fileEnding = '.webp'
+                }
+                var num = i + 1
+                var fileName = imgNamePattern + ' ' + num + fileEnding
+                // Add to array
+                exportedFiles.push([fileName, blob])
+            })
+            console.log(exportedFiles)
+        })
+    })
+}
+
 function updateSampleFileNames() {
     var text = document.getElementById('photostack-name-pattern').value
     if (text === '') {
@@ -110,7 +173,11 @@ document.getElementById('photostack-name-pattern').addEventListener('keyup', fun
 
 // Export button in modal
 document.getElementById('photostack-export-zip-btn').addEventListener('click', function () {
-    createZip()
+    if (Modernizr.promises) {
+        asyncExport()
+    } else {
+        legacyExport()
+    }
 })
 
 // Reset modal content when the close button is clicked
