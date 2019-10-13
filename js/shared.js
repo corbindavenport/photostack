76d610e237ddec125898b099d6b35a061bf731d4
Corbin Davenport <corbin@pop-os.localdomain>
2019-10-13 15:23:56 -0400

    Hide donate links for paid Android app

---
+++ js/shared.js
@@ -0,0 +1,7 @@
+// Hide donate links for the Play Store app
+if ((localStorage['paid'] === 'true') || document.referrer.includes('android-app://')) {
+    if (!(localStorage['paid']) === 'true') {
+        localStorage['paid'] = 'true'
+    }
+    document.getElementsByTagName('html')[0].classList.add('photostack-paid')
+}
