9ee68c1e7976fd4f9b066e786f124980f6092403
Corbin Davenport <me@corbin.io>
2024-04-24 02:18:10 -0400

    Add theme switcher, change default theme

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,38 @@
 const editorMode = document.getElementsByTagName('html')[0].dataset.photostackMode
 
+// Light and dark mode switching
+function applyTheme() {
+    if (localStorage.getItem('theme') === 'light') {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+    } else if (localStorage.getItem('theme') === 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+        }
+    }
+}
+
+// Add click events to theme switcher after the page has loaded
+window.addEventListener('load', function() {
+    document.querySelectorAll('.photostack-theme-btn').forEach(function (el) {
+        el.addEventListener('click', function() {
+            localStorage.setItem('theme', el.dataset.theme);
+            applyTheme();
+        })
+    })
+})
+
+// Handle changes to system theme
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function() {
+    applyTheme();
+})
+
+// Apply theme on initial page load
+applyTheme();
+
 // Apply settings to a canvas
 function applyCanvasSettings(canvas, watermarkObject = null, previewMode = false) {
     return new Promise(async function (resolve) {
